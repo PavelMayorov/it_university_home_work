a0f7c209460e01bd5f15088c9e0174ca66e4ac44
pavel <pavelmayorov1996@gmail.com>
2021-11-12 21:29:56 +0500

    коммит

---
+++ lesson_2.py
@@ -0,0 +1,8 @@
+user_info_in_str = ['pavel', 'mayorov', '24', 'perm', 'engineer']
+user_info_in_tuple = {
+    'first_name': 'pavel',
+    'last_name': 'mayorov',
+    'age': 24,
+    'city': 'perm',
+    'profession': 'engineer',
+}
