871c0edaa6d47552e4bb4cf688f642ad4c179884
pavel <pavelmayorov1996@gmail.com>
2021-11-16 22:00:30 +0500

    исправление имени словаря

--- lesson_3.py
+++ lesson_3.py
@@ -1,28 +1,28 @@
-users_info_in_str = ['pavel', 'mayorov', 24, 'perm', 'engineer', 'vladimir', 'putin', 69, 'moscow', 'president',
+users_info_in_list = ['pavel', 'mayorov', 24, 'perm', 'engineer', 'vladimir', 'putin', 69, 'moscow', 'president',
                      'dmitriy', 'medvedev', 56, 'moscow', 'vicepresident']
 
 users_info_in_dict = {'user_1': {'first_name': 'pavel',
-                                  'last_name': 'mayorov',
-                                  'age': 24,
-                                  'city': 'perm',
-                                  'profession': 'engineer',
+                                 'last_name': 'mayorov',
+                                 'age': 24,
+                                 'city': 'perm',
+                                 'profession': 'engineer',
                                  },
-                       'user_2': {'first_name': 'vladimir',
-                                  'last_name': 'putin',
-                                  'age': 69,
-                                  'city': 'moscow',
-                                  'profession': 'president',
-                                  },
+                      'user_2': {'first_name': 'vladimir',
+                                 'last_name': 'putin',
+                                 'age': 69,
+                                 'city': 'moscow',
+                                 'profession': 'president',
+                                 },
                       'user_3': {'first_name': 'dmitriy',
-                                  'last_name': 'medvedev',
-                                  'age': 56,
-                                  'city': 'moscow',
-                                  'profession': 'president',
-                                  },
+                                 'last_name': 'medvedev',
+                                 'age': 56,
+                                 'city': 'moscow',
+                                 'profession': 'president',
+                                 },
                       }
 
 # Поиск самого старшего пользователя
-age_all_users = users_info_in_str[2::5]
+age_all_users = users_info_in_list[2::5]
 print(sorted(age_all_users, reverse=True)[0])
 
 age_all_users = []
